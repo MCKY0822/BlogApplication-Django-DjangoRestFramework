70786f0ceef0eae840df47671f308bc41652b076
Michael Montana <michaelmontana@me.com>
2024-10-13 19:01:43 +0800

    feat: implement simple user login

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import BlogListCreateView, BlogRetrieveDeleteView
+from .views import BlogListCreateView, BlogRetrieveDeleteView, login_view, home_view 
 
 urlpatterns = [
     path('blogs/', BlogListCreateView.as_view(), name='blog-list-create'),
     path('blogs/<int:id>/', BlogRetrieveDeleteView.as_view(), name='blog-retrieve-delete'),
+    path('login/', login_view, name='login'),
+    path('home/', home_view, name='home'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from rest_framework import generics, permissions
 from .models import Blog
 from .serializers import BlogSerializer
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 
 class BlogListCreateView(generics.ListCreateAPIView):  # List and Create Blog Posts
@@ -18,3 +22,25 @@ class BlogRetrieveDeleteView(generics.RetrieveDestroyAPIView):  # Retrieve and D
     serializer_class = BlogSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     lookup_field = 'id'
+
+
+@login_required
+def home_view(request):
+    return render(request, 'home.html')
+
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)  # Log the user in
+            return redirect('home')  # Redirect to the home page (or any other page)
+        else:
+            # If login fails, display an error message
+            return render(request, 'login.html', {'error_message': 'Invalid username or password'})
+    
+    return render(request, 'login.html')
