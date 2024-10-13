0a77edae9ab2058fcca1dcecb499f754b7c47212
Michael Montana <michaelmontana@me.com>
2024-10-13 18:14:35 +0800

    fix: blog retrieve delete

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from . views import BlogListCreateView, BlogDeleteView
+from .views import BlogListCreateView, BlogRetrieveDeleteView
 
 urlpatterns = [
     path('blogs/', BlogListCreateView.as_view(), name='blog-list-create'),
-    path('blogs/<int:id>/', BlogDeleteView.as_view(), name='blog-delete'),
+    path('blogs/<int:id>/', BlogRetrieveDeleteView.as_view(), name='blog-retrieve-delete'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -4,17 +4,17 @@ from .models import Blog
 from .serializers import BlogSerializer
 
 
-class BlogListCreateView(generics.ListCreateAPIView): # List and Create Blog Posts
+class BlogListCreateView(generics.ListCreateAPIView):  # List and Create Blog Posts
     queryset = Blog.objects.all()
     serializer_class = BlogSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def perform_create(self, serializer):
-        serializer.save(author=self.request.user) # Set the author to the currently logged-in user
+        serializer.save(author=self.request.user)  # Set the author to the currently logged-in user
 
 
-class BlogDeleteView(generics.DestroyAPIView): # Delete Blog Post
+class BlogRetrieveDeleteView(generics.RetrieveDestroyAPIView):  # Retrieve and Delete Blog Post
     queryset = Blog.objects.all()
     serializer_class = BlogSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     lookup_field = 'id'
