c78c31c425537616ad2ebe8ef1d820db263ca74b
Michael Montana <michaelmontana@me.com>
2024-10-13 14:26:55 +0800

    feat: new app and createsuperuser

---
+++ blog/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Blog
+
+
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created_at')  # to display in the list view
+    list_filter = ('author', 'created_at') # to filter by in the admin panel
+    search_fields = ('title', 'content', 'author__username') # to search in the admin panel
+
+
+admin.site.register(Blog, BlogAdmin) # Register the Blog model with the custom admin class
