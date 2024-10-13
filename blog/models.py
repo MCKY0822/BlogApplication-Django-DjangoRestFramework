462923ed544d64827f5e5c0e028cc4bcd6d48798
Michael Montana <michaelmontana@me.com>
2024-10-13 16:43:16 +0800

    add comment for documentation

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# create model as per requirements
+# create model as per client's requirements
 class Blog(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
